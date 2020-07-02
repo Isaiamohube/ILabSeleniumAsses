45c02ae9b8b4e891fced2728274107d7bdf0e921
Isaiamohube <67730043+Isaiamohube@users.noreply.github.com>
2020-07-02 09:55:38 +0200

    Add files via upload

---
+++ SetMethods.cs
@@ -0,0 +1,129 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace New1
+{
+    class SetMethods
+    {
+        //Enter text
+
+        public static void EnterText(IWebDriver driver, string Object, string value, string objectType)
+        {
+            if (objectType == "Id")
+                driver.FindElement(By.Id(Object)).SendKeys(value);
+            if (objectType == "name")
+                driver.FindElement(By.Name(Object)).SendKeys(value);
+            if (objectType == "XPath")
+                driver.FindElement(By.XPath(Object)).SendKeys(value);
+        }
+
+        // clicking a button, checkbox or option
+
+        public static void Click(IWebDriver driver, string Object, string objectType)
+        {
+            if (objectType == "Id")
+                driver.FindElement(By.Id(Object)).Click();
+            if (objectType == "name")
+                driver.FindElement(By.Name(Object)).Click();
+            if (objectType == "XPath")
+                driver.FindElement(By.XPath(Object)).Click();
+        }
+
+        //Selecting a drop down
+
+        public static void Selecting(IWebDriver driver, string Object, string value, string objectType)
+        {
+            if (objectType == "Id")
+                new SelectElement(driver.FindElement(By.Id(Object))).SelectByText(value);
+            if (objectType == "name")
+                new SelectElement(driver.FindElement(By.Name(Object))).SelectByText(value);
+            if (objectType == "XPath")
+                new SelectElement(driver.FindElement(By.XPath(Object))).SelectByText(value);
+        }
+
+        //Highlighting
+
+        public static void highlightObj(IWebDriver driver, IWebElement element)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+                js.ExecuteScript("arguments[0].setAttribute('style',arguments[1]);", element, "color:yellow;border:2px solid yellow;");
+                js.ExecuteScript("arguments[0].setAttribute('style',arguments[1];", element, "");
+
+            }
+
+        }
+
+    
+
+        public static void TellUsAbtYourCarPositive(IWebDriver driver)
+        {
+
+             driver.FindElement(By.Name("vehicle_one_description")).SendKeys("2014 VOLKSWAGEN POLO VIVO 1.4");         
+             System.Threading.Thread.Sleep(2000);
+             driver.FindElement(By.Name("vehicle_one_description")).SendKeys(Keys.Down);
+             System.Threading.Thread.Sleep(2000);
+             driver.FindElement(By.Name("vehicle_one_description")).SendKeys(Keys.Enter);
+             System.Threading.Thread.Sleep(2000);
+             SetMethods.Selecting(driver, "/html/body/div[10]/div/div[2]/div/div[1]/div[2]/div[2]/div[1]/select", "LOCKED GARAGE", "XPath");
+
+          
+
+            Screenshot s7 = ((ITakesScreenshot)driver).GetScreenshot();
+            s7.SaveAsFile(@"C:\\Users\\mo5599\\Documents\Visual Studio 2013\\Projects\\New1\\New1\\Images\\S7.jpg");
+
+            IJavaScriptExecutor jx = (IJavaScriptExecutor)driver;
+            jx.ExecuteScript("arguments[0].click()", driver.FindElement(By.XPath("/html/body/div[10]/div/div[2]/div/div[2]/button[2]")));
+
+        }
+
+        public static void PersonalDetailsFill(IWebDriver driver)
+        {
+             SetMethods.Click(driver, "/html/body/div[11]/div/div[2]/div/div[1]/div[2]/div[2]/div[2]/button/img", "XPath");
+             SetMethods.EnterText(driver, "//*[@id='person-details-man']/div/div[1]/div[2]/div[1]/div/div/input", "John", "XPath");
+             SetMethods.EnterText(driver, "//*[@id='person-details-man']/div/div[1]/div[2]/div[2]/div/div/input", "Smith", "XPath");
+             SetMethods.EnterText(driver, "dayOfBirth-man", "01", "name");
+             SetMethods.EnterText(driver, "monthOfBirth-man", "05", "name");
+             SetMethods.EnterText(driver, "yearOfBirth-man", "1970", "name");
+             SetMethods.Selecting(driver, "//*[@id='person-details-man']/div/div[1]/div[2]/div[4]/div/select", "Single", "XPath");
+             SetMethods.EnterText(driver, "cell", "0815699088", "name");
+             SetMethods.EnterText(driver, "email", "johnSmith@test.com", "name");
+             SetMethods.Click(driver, "//*[@id='person-details-man']/div/div[1]/div[2]/div[7]/label[2]/i", "XPath");
+             SetMethods.EnterText(driver, "//*[@id='address-man']", "9th Rd, Midrand", "XPath");
+        }
+
+        public static void TellUsAbtRegularDriverPos(IWebDriver driver)
+        {
+            SetMethods.EnterText(driver, "/html/body/div[14]/div/div[2]/div/div[1]/div/div[2]/div[1]/div/div/input[1]", "02", "XPath");
+            SetMethods.EnterText(driver, "/html/body/div[14]/div/div[2]/div/div[1]/div/div[2]/div[1]/div/div/input[2]", "1990", "XPath");
+            SetMethods.Selecting(driver, "/html/body/div[14]/div/div[2]/div/div[1]/div/div[2]/div[2]/div/select", "RSA Drivers Licence", "XPath");
+            SetMethods.Selecting(driver, "/html/body/div[14]/div/div[2]/div/div[1]/div/div[2]/div[3]/div/select", "2 Years Comprehensive Insurance", "XPath");
+        }
+        public static void ChatBotNavigation(IWebDriver driver)
+        {
+
+            SetMethods.Click(driver, "//*[@id='floatingButtonDiv']/img", "XPath");
+            SetMethods.Click(driver, "//*[@id='innerChatButton']/img", "XPath");
+            SetMethods.EnterText(driver, "//*[@id='name_Field']", "John", "XPath");
+            SetMethods.EnterText(driver, "//*[@id='surname_Field']", "Ford", "XPath");
+            SetMethods.EnterText(driver, "//*[@id='email_Field']", "John@test.com", "XPath");
+            SetMethods.EnterText(driver, "//*[@id='ContactNo_Field']", "0839677814", "XPath");
+            SetMethods.Click(driver, "//*[@id='startButton']", "XPath");
+            System.Threading.Thread.Sleep(3000);
+
+        }
+
+
+
+
+
+
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using New1.TestPagesObjects;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using System;
+using excel = Microsoft.Office.Interop.Excel;
+
+namespace New1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        protected IWebDriver driver;
+
+        private JobApplication jobApp = new JobApplication();
+
+        private objectRepositary objectRep = new objectRepositary();
+
+        [TestInitialize]
+        public void Initialise()
+        {
+            string browserName = "Chrome";
+
+            if (browserName.Equals("Chrome"))
+            {
+                driver = new ChromeDriver(@"C:\Workspace\Hippo\Frontend_Auto\Exe");
+            }
+            else if (browserName.Equals("IE"))
+            {
+                driver = new InternetExplorerDriver(@"C:\Workspace\Hippo\Frontend_Auto\IE Driver");
+            }
+
+            driver.Navigate().GoToUrl("https://www.ilabquality.com/");
+
+            driver.Manage().Window.Maximize();
+        }
+
+        [TestMethod]
+        public void validateUploadError()
+        {
+            excel.Application x1App = new excel.Application();
+            excel.Workbook x1WorkBook = x1App.Workbooks.Open(@"C:\Workspace\Hippo\Frontend_Auto\Excel\iLab.xlsx");
+            excel._Worksheet x1WorkSheet = x1WorkBook.Sheets[1];
+            excel.Range x1Range = x1WorkSheet.UsedRange;
+            int xlRowCnt;
+
+            for (xlRowCnt = 2; xlRowCnt <= x1Range.Rows.Count; xlRowCnt++)
+            {
+                jobApp.country = (string)(x1Range.Cells[xlRowCnt, 2] as excel.Range).Value2;
+                jobApp.applicantName = (string)(x1Range.Cells[xlRowCnt, 3] as excel.Range).Value2;
+                jobApp.applicantEmail = (string)(x1Range.Cells[xlRowCnt, 4] as excel.Range).Value2;
+
+                Random rnd = new Random();
+                var tempCell = rnd.Next(99999999);
+                jobApp.applicantCelNumber = String.Format("{0:0## ### ####}", tempCell);
+
+                objectRep.clickCarrers(driver);
+                objectRep.selectCountry(driver, jobApp.country);
+                objectRep.clickFirstJobOpenning(driver);
+                objectRep.clickApplyNow(driver);
+                objectRep.typeApplicantName(driver, jobApp.applicantName);
+                objectRep.typeApplicantEmail(driver, jobApp.applicantEmail);
+
+                objectRep.typeApplicantCellNumber(driver, jobApp.applicantCelNumber.ToString());
+                objectRep.sendApplication(driver);
+
+                string expectedUploadError = "You need to upload at least one file.";
+
+                IWebElement actualUploadErrorField = driver.FindElement(By.XPath("//*[@id='wpjb-apply-form']/fieldset[1]/div[5]/div/ul/li"));
+
+                string actualUploadError = actualUploadErrorField.Text;
+
+                // NUnit.Framework.Assert.AreEqual(expectedUploadError, actualUploadError);
+
+                (x1Range.Cells[xlRowCnt, 5] as excel.Range).Value2 = jobApp.applicantCelNumber;
+
+                if (expectedUploadError == actualUploadError)
+                {
+                    (x1Range.Cells[xlRowCnt, 6] as excel.Range).Value2 = "Pass";
+                }
+                else
+                {
+                    (x1Range.Cells[xlRowCnt, 6] as excel.Range).Value2 = "False";
+                }
+            }
+        }
+
+        [TestCleanup]
+        public void cleanUp()
+        {
+            System.Threading.Thread.Sleep(10000);
+
+            driver.Close();
+        }
+    }
+}
